0c3054822f4b8b633dfcb2c6f93519a079200637
charles <charles@charlescarden.com>
2023-01-02 00:09:22 -0600

    Change program.cs to ATMApp.cs

---
+++ ATMApp/ATMApp.cs
@@ -0,0 +1,17 @@
+﻿namespace ATMApp
+{
+    internal class ATMApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Title = "ATM Application";
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.WriteLine("Welcome to ATM APP");
+            Console.WriteLine("Please insert ATM Card");
+
+            Console.WriteLine("\n\nPress Enter to continue...\n\n");
+        }
+    }
+}
